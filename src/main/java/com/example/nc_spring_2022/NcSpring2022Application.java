c587dce865d5388eec8d74e0abbb5f1bb59f0582
sloning <i.96petrov@gmail.com>
2022-03-11 20:15:18 +0300

    project structure

---
+++ src/main/java/com/example/nc_spring_2022/NcSpring2022Application.java
@@ -0,0 +1,13 @@
+package com.example.nc_spring_2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcSpring2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcSpring2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nc_spring_2022/NcSpring2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nc_spring_2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcSpring2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
